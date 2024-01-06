03054bbea61e85b39f92355d1779764c61a2da2b
beankhanh <beankhanh2001@gmail.com>
2024-01-06 16:32:59 +0700

    endpoints & BCryptPassword

--- src/main/java/vn/khanh/webbansach_backend/service/TaiKhoanService.java
+++ src/main/java/vn/khanh/webbansach_backend/service/TaiKhoanService.java
@@ -2,6 +2,7 @@ package vn.khanh.webbansach_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import vn.khanh.webbansach_backend.dao.NguoiDungRepository;
 import vn.khanh.webbansach_backend.entity.NguoiDung;
@@ -9,6 +10,8 @@ import vn.khanh.webbansach_backend.entity.ThongBao;
 
 @Service
 public class TaiKhoanService {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     @Autowired
     private NguoiDungRepository nguoiDungRepository;
     public ResponseEntity<?> dangKyNguoiDung(NguoiDung nguoiDung){
@@ -18,6 +21,8 @@ public class TaiKhoanService {
         if(nguoiDungRepository.existsByEmail(nguoiDung.getEmail())){
             return ResponseEntity.badRequest().body(new ThongBao("Tên Email đã tồn tại"));
         }
+        String encryptPassword = passwordEncoder.encode(nguoiDung.getMatKhau());
+        nguoiDung.setMatKhau(encryptPassword);
         NguoiDung nguoiDung1=nguoiDungRepository.save(nguoiDung);
         return ResponseEntity.ok("Đăng ký thành công");
     }
